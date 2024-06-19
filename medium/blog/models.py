de945e9e259d2658b9508b3255cab61f7d4c35f6
Ismat <ismetsemedov@gmail.com>
2024-06-19 19:31:21 +0400

    check

---
+++ medium/blog/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.utils import timezone
+from django.core.urlresolvers import reverse
+
+# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.Charfield(max_length = 200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default = timezone.now())
+    published_date = models.DateTimeField(blank=True, null=True)
+    
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+        
+    def approve_comments(self):
+        return self.comments.filter(approved_comments = True)
+    
+    def get_absolute_url(self):
+        return reverse("post_details", kwargs = {'pk':self.pk})    
+    
+    def __str__(self):
+        return self.title
+    
+class Comment(model.Model):
+    post = models.ForeignKey('blog.Post',related_name = 'comments')
+    author_name = models.CharField(max_length = 200)
+    text = models.TextField()
+    create_date = models.DateTimeField(defaut = timezone.now())
+    approved_comment = models.BooleanField(default = False)
+    
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+    
+    def get_absolute_url(self):
+        return reverse("post_list")
+            
+        
+    def __str__(self):
+        return self.text
+    
+        
+        
+    
+        
+        
