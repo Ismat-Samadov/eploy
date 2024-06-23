8cc16b8c46966e1d49dff9bdad77e5f8ac426602
Ismat <ismetsemedov@gmail.com>
2024-06-23 15:39:28 +0400

    views and urls

--- medium/blog/models.py
+++ medium/blog/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.utils import timezone
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
-    title = models.Charfield(max_length = 200)
+    author = models.ForeignKey('auth.User',on_delete=models.PROTECT)
+    title = models.CharField(max_length = 200)
     text = models.TextField()
     create_date = models.DateTimeField(default = timezone.now())
     published_date = models.DateTimeField(blank=True, null=True)
@@ -23,11 +23,11 @@ class Post(models.Model):
     def __str__(self):
         return self.title
     
-class Comment(model.Model):
-    post = models.ForeignKey('blog.Post',related_name = 'comments')
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post',related_name = 'comments',on_delete=models.PROTECT)
     author_name = models.CharField(max_length = 200)
     text = models.TextField()
-    create_date = models.DateTimeField(defaut = timezone.now())
+    create_date = models.DateTimeField(default = timezone.now())
     approved_comment = models.BooleanField(default = False)
     
     def approve(self):
--- medium/blog/views.py
+++ medium/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import (TemplateView)
+
 
 # Create your views here.
+class AboutView(TemplateView):
+    template_name = 'about.html'
+    
