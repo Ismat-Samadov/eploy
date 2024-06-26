162a0c4a11e01dda9d2f60e3fc917ab4184b7c31
Ismat <ismetsemedov@gmail.com>
2024-06-26 19:42:59 +0400

    check

--- medium/blog/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import (TemplateView)
-
-
-# Create your views here.
-class AboutView(TemplateView):
-    template_name = 'about.html'
-    
