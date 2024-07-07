a573904454047cb752aa27234be6d7f2b099ab86
Ismat <ismetsemedov@gmail.com>
2024-07-07 16:15:42 +0400

    check

--- jobsite/jobsite/settings.py
+++ jobsite/jobsite/settings.py
@@ -113,3 +113,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+)
