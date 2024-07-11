9fc9d4544e8407c4f3c43e78da6853dc2d6f48d8
Ismat <ismetsemedov@gmail.com>
2024-07-11 16:23:19 +0400

    check

--- jobsite/jobs/views.py
+++ jobsite/jobs/views.py
@@ -19,53 +19,6 @@ logger = logging.getLogger(__name__)
 def redirect_to_jobs(request):
     return redirect('fetch_jobs_from_api')
 
-# @login_required
-# def user_dashboard(request):
-#     if request.user.user_type == 'HR':
-#         jobs = JobPost.objects.filter(posted_by=request.user, deleted=False)
-#         applications = JobApplication.objects.filter(job__posted_by=request.user)
-#         return render(request, 'jobs/hr_dashboard.html', {'jobs': jobs, 'applications': applications})
-#     elif request.user.user_type == 'Candidate':
-#         applications = JobApplication.objects.filter(applicant=request.user)
-#         return render(request, 'jobs/candidate_dashboard.html', {'applications': applications})
-#     else:
-#         return HttpResponseForbidden("You are not authorized to view this page.")
-# @login_required
-# def user_dashboard(request):
-#     if request.user.user_type == 'HR':
-#         jobs = JobPost.objects.filter(posted_by=request.user, deleted=False)
-#         applications = JobApplication.objects.filter(job__posted_by=request.user)
-#         return render(request, 'jobs/hr_dashboard.html', {'jobs': jobs, 'applications': applications})
-#     elif request.user.user_type == 'Candidate':
-#         applications = JobApplication.objects.filter(applicant=request.user)
-#         return render(request, 'jobs/candidate_dashboard.html', {'applications': applications})
-#     else:
-#         return HttpResponseForbidden("You are not authorized to view this page.")
-# @login_required
-# def user_dashboard(request):
-#     if request.user.user_type == 'HR':
-#         jobs = JobPost.objects.filter(posted_by=request.user, deleted=False)
-#         applications = JobApplication.objects.filter(job__posted_by=request.user).select_related('job', 'applicant')
-#         return render(request, 'jobs/hr_dashboard.html', {'jobs': jobs, 'applications': applications})
-#     elif request.user.user_type == 'Candidate':
-#         applications = JobApplication.objects.filter(applicant=request.user).select_related('job', 'applicant')
-#         return render(request, 'jobs/candidate_dashboard.html', {'applications': applications})
-#     else:
-#         return HttpResponseForbidden("You are not authorized to view this page.")
-
-# @login_required
-# def user_dashboard(request):
-#     if request.user.user_type == 'HR':
-#         jobs = JobPost.objects.filter(posted_by=request.user, deleted=False)
-#         applications = JobApplication.objects.filter(job__posted_by=request.user).select_related('job', 'applicant')
-#         return render(request, 'jobs/hr_dashboard.html', {'jobs': jobs, 'applications': applications})
-#     elif request.user.user_type == 'Candidate':
-#         applications = JobApplication.objects.filter(applicant=request.user).select_related('job', 'applicant')
-#         return render(request, 'jobs/candidate_dashboard.html', {'applications': applications})
-#     else:
-#         return HttpResponseForbidden("You are not authorized to view this page.")
-
-
 @login_required
 def user_dashboard(request):
     if request.user.user_type == 'HR':
