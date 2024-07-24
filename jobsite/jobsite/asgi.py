94aa83ae70275a196a0312a8ea4b03154cefc602
Ismat <ismetsemedov@gmail.com>
2024-07-24 20:10:51 +0400

    chat app

--- jobsite/jobsite/asgi.py
+++ jobsite/jobsite/asgi.py
@@ -1,19 +1,7 @@
 import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import chat.routing
+import django
+from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobsite.settings')
-
-# Initialize Django ASGI application early to ensure the AppRegistry is populated.
-django_asgi_app = get_asgi_application()
-
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
-    ),
-})
+django.setup()
+application = get_default_application()
--- jobsite/jobsite/routing.py
+++ jobsite/jobsite/routing.py
@@ -1,6 +1,5 @@
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
 import chat.routing
 
 application = ProtocolTypeRouter({
