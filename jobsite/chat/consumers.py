11972dcba79add1c77039879d71e3819cc023f94
Ismat <ismetsemedov@gmail.com>
2024-07-24 16:48:54 +0400

    chat app

--- jobsite/chat/consumers.py
+++ jobsite/chat/consumers.py
@@ -1,13 +1,16 @@
-import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+import json
 from .models import Room, Message
-
+from django.contrib.auth import get_user_model
+import logging
+logger = logging.getLogger(__name__)
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
+        self.room_group_name = f'chat_{self.room_name}'
 
+        # Join room group
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
@@ -16,6 +19,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
+        # Leave room group
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
@@ -24,7 +28,15 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        user = self.scope["user"]
+        logger.info(f"Received message: {message} from user: {user}")
 
+        # Save message to database
+        room = await database_sync_to_async(Room.objects.get)(slug=self.room_name)
+        await database_sync_to_async(Message.objects.create)(room=room, user=user, content=message)
+        logger.info(f"Message saved to database: {message_instance}")
+
+        # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
@@ -36,6 +48,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def chat_message(self, event):
         message = event['message']
 
+        # Send message to WebSocket
         await self.send(text_data=json.dumps({
             'message': message
         }))
