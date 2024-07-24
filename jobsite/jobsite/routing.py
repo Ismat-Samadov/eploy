3a8dcc16c6fe306610dbdc24e5d850cde0d27db8
Ismat <ismetsemedov@gmail.com>
2024-07-24 19:40:06 +0400

    chat app

--- jobsite/jobsite/routing.py
+++ jobsite/jobsite/routing.py
@@ -1,10 +1,11 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
 import chat.routing
 
 application = ProtocolTypeRouter({
-    'http': get_default_application(),  # Add HTTP URL routes here if any
-    'websocket': AuthMiddlewareStack(
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
         )
