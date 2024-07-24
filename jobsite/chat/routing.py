4bf922125efbf75189da5bfa4a7d722457ba63eb
Ismat <ismetsemedov@gmail.com>
2024-07-24 18:12:06 +0400

    chat app

--- jobsite/chat/routing.py
+++ jobsite/chat/routing.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import consumers
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from .consumers import ChatConsumer
 
 websocket_urlpatterns = [
-    path('ws/chat/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
+    path('ws/chat/<str:room_name>/', ChatConsumer.as_asgi()),
 ]
--- jobsite/jobsite/routing.py
+++ jobsite/jobsite/routing.py
@@ -1,12 +1,14 @@
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-import chat.routing
+from django.urls import path
+from channels.auth import AuthMiddlewareStack
+from . import consumers
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
+        URLRouter([
+            path("ws/chat/<str:room_name>/", consumers.ChatConsumer.as_asgi()),
+        ])
     ),
 })
+
