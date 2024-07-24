42190820204a5919bf8001589420d993d351f5f4
Ismat <ismetsemedov@gmail.com>
2024-07-24 19:27:25 +0400

    chat app

--- jobsite/jobsite/routing.py
+++ jobsite/jobsite/routing.py
@@ -5,7 +5,7 @@ from channels.auth import AuthMiddlewareStack
 from . import consumers
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    # "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter([
             path("ws/chat/<str:room_name>/", consumers.ChatConsumer.as_asgi()),
