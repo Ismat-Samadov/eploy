fb56d943564e75274b99c908288c86a4f35d87a6
Ismat <ismetsemedov@gmail.com>
2024-07-24 16:17:10 +0400

    chat app

--- jobsite/chat/consumers.py
+++ jobsite/chat/consumers.py
@@ -2,15 +2,17 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from .models import Room, Message
 
+
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = f'chat_{self.room_name}'
+        self.room_group_name = 'chat_%s' % self.room_name
 
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
+
         await self.accept()
 
     async def disconnect(self, close_code):
@@ -23,24 +25,17 @@ class ChatConsumer(AsyncWebsocketConsumer):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
-        user = self.scope['user']
-        room, created = Room.objects.get_or_create(name=self.room_name)
-        Message.objects.create(user=user, content=message, room=room)
-
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message,
-                'user': user.username,
+                'message': message
             }
         )
 
     async def chat_message(self, event):
         message = event['message']
-        user = event['user']
 
         await self.send(text_data=json.dumps({
-            'message': message,
-            'user': user,
+            'message': message
         }))
