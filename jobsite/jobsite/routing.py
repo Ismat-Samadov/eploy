a968b673acdcb908db12dfd382d660f663a39bde
Ismat <ismetsemedov@gmail.com>
2024-07-24 20:33:59 +0400

    chat app

--- jobsite/jobsite/routing.py
+++ jobsite/jobsite/routing.py
@@ -1,5 +1,6 @@
-from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
 import chat.routing
 
 application = ProtocolTypeRouter({
