e3667ae081480591163117fffc9d06c5a945e661
Ismat <ismetsemedov@gmail.com>
2024-07-27 18:16:04 +0400

    upated for adsense

--- jobsite/jobs/sitemaps.py
+++ jobsite/jobs/sitemaps.py
@@ -6,10 +6,24 @@ from .models import JobPost
 
 class JobSitemap(Sitemap):
     changefreq = "daily"
-    priority = 0.8
+    priority = 0.9
 
     def items(self):
         return JobPost.objects.all()
 
+    def lastmod(self, obj):
+        return obj.updated_at
+
     def location(self, obj):
-        return reverse('job_detail', args=[obj.pk])
+        return reverse('job_detail', args=[obj.id])
+
+# Add more sitemaps if needed, for other models or static views
+class StaticViewSitemap(Sitemap):
+    priority = 0.5
+    changefreq = 'weekly'
+
+    def items(self):
+        return ['about', 'job_list', 'home']
+
+    def location(self, item):
+        return reverse(item)
--- jobsite/jobsite/urls.py
+++ jobsite/jobsite/urls.py
@@ -7,20 +7,22 @@ from django.conf.urls.static import static
 from django.views.generic import RedirectView
 from django.contrib.sitemaps.views import sitemap
 from jobs.views import redirect_to_jobs, test_openai_api
-from jobs.sitemaps import JobSitemap
+from jobs.sitemaps import JobSitemap, StaticViewSitemap
+
 
 sitemaps = {
     'jobs': JobSitemap,
+    'static': StaticViewSitemap,
 }
 
 urlpatterns = [
-    path('', redirect_to_jobs, name='home'),
+    path('', RedirectView.as_view(url='/jobs/', permanent=True)),
     path('admin/', admin.site.urls),
     path('jobs/', include('jobs.urls')),
-    path('test-openai/', test_openai_api, name='test_openai_api'),
-    re_path(r'^ads.txt$', RedirectView.as_view(url=settings.STATIC_URL + 'ads.txt', permanent=False)),
     re_path(r'^robots.txt$', RedirectView.as_view(url=settings.STATIC_URL + 'robots.txt', permanent=False)),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('test-openai/', test_openai_api, name='test_openai_api'),
+    re_path(r'^ads.txt$', RedirectView.as_view(url=settings.STATIC_URL + 'ads.txt', permanent=False)),
 ]
 
 # Serve media files during development
