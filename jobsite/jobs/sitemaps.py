316972f623fb9f3ec73274849c9f4e1a7fdcdc99
Ismat <ismetsemedov@gmail.com>
2024-07-27 18:06:37 +0400

    upated for adsense

--- jobsite/jobs/sitemaps.py
+++ jobsite/jobs/sitemaps.py
@@ -2,14 +2,14 @@
 
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
-from .models import Job
+from .models import JobPost
 
 class JobSitemap(Sitemap):
     changefreq = "daily"
     priority = 0.8
 
     def items(self):
-        return Job.objects.all()
+        return JobPost.objects.all()
 
     def location(self, obj):
         return reverse('job_detail', args=[obj.pk])
