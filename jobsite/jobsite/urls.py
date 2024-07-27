608dd824847b85d759eefabc8bc136ad2d096dd9
Ismat <ismetsemedov@gmail.com>
2024-07-27 17:50:59 +0400

    upated for adsense

--- jobsite/jobsite/urls.py
+++ jobsite/jobsite/urls.py
@@ -1,9 +1,8 @@
-# jobsite/urls.py
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 from jobs.views import redirect_to_jobs, test_openai_api
 
 urlpatterns = [
@@ -11,6 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('jobs/', include('jobs.urls')),
     path('test-openai/', test_openai_api, name='test_openai_api'),
+    re_path(r'^ads.txt$', RedirectView.as_view(url=settings.STATIC_URL + 'ads.txt', permanent=False)),
 ]
 
 # Serve media files during development
