ff30aefe055f3b59195b2542f96b1d293f72cccd
Ismat <ismetsemedov@gmail.com>
2024-08-23 08:50:19 +0400

    fixed superuser

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,20 +1,15 @@
 # jobs/admin.py
-
 from django.contrib import admin
 from .models import JobPost, JobApplication
 
 @admin.register(JobPost)
 class JobPostAdmin(admin.ModelAdmin):
     list_display = ('title', 'company', 'location', 'posted_by', 'posted_at')
-    search_fields = ('title', 'company', 'location', 'posted_by__username')
+    search_fields = ('title', 'company', 'location', 'posted_by__email')
     list_filter = ('posted_at', 'location', 'company')
 
 @admin.register(JobApplication)
 class JobApplicationAdmin(admin.ModelAdmin):
-    list_display = ('job', 'get_applicant_name', 'applied_at')
-    search_fields = ('job__title', 'applicant__username')
+    list_display = ('job', 'full_name', 'applied_at')
+    search_fields = ('job__title', 'full_name', 'email')
     list_filter = ('applied_at', 'job__title')
-
-    def get_applicant_name(self, obj):
-        return obj.applicant.get_full_name() if obj.applicant.get_full_name() else obj.applicant.username
-    get_applicant_name.short_description = 'Applicant'
--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
 
 class CustomUser(AbstractUser):
     USER_TYPE_CHOICES = (
@@ -12,6 +13,8 @@ class CustomUser(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
 
     def __str__(self):
         return self.email
